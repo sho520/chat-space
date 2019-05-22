faa2d45385e29556bdd12f2f7abbabe56eb4b72c
sho520 <shoa.xyz@gmail.com>
2019-05-22 21:29:41 +0900

    Revert "a"
    This reverts commit 2c10b4a8b901370b6f5e0a1b650fd8c8b96f83b4.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,12 +1,4 @@
 class MessagesController < ApplicationController
     def index
-<<<<<<< HEAD
     end
-=======
-    
-    end
-
-    def create
-    end
->>>>>>> parent of b52d11c... 5/22 20:53 非同期通信前
 end
