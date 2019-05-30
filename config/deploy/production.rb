49478458d754fb96da3678a8b237a792462013e6
sho520 <shoa.xyz@gmail.com>
2019-05-30 13:19:32 +0900

    修正２

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -63,26 +63,3 @@
 server '54.238.178.19', user: 'ec2-user', roles: %w{app db web}
 
 # config valid only for current version of Capistrano
-lock '3.11.0'
-
-set :application, 'chat-space'
-set :repo_url,  'git@github.com:sho520/chat-space.git'
-
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
-
-set :rbenv_type, :user
-set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
-
-set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['/Users/sho/.ssh/curryrice.pem']  ※例：~/.ssh/key_pem.pem
-
-set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
-set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
-set :keep_releases, 5
-
-after 'deploy:publishing', 'deploy:restart'
-namespace :deploy do
-  task :restart do
-    invoke 'unicorn:restart'
-  end
-end
